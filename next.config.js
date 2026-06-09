e71a3f292bb0da8bbe33d82aae1f40c7233acfe7
Albert Boersen <boerdt.dongen@gmail.com>
2026-06-09 21:45:51 +0200

    Upgrade nextjs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals = config.externals || [];
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
